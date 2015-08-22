9140c90da1b8da95fb835a07deefed685da78521
cat12079801 <cat12079801@gmail.com>
2015-08-22 11:41:57 +0900

    routesをとりあえず書いた

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "problems#index"
 
+  resources :problems
+
+  scope :admin do
+    resources :admin_notifications, :path => 'notifications'
+    resources :admin_problems, :path => 'problems'
+  end
+
   get 'admin/index'
-
   get 'admin/flag'
-
   get 'admin/score'
-
   get 'admin/genre'
-
   get 'admin/genre_post'
-
   get 'admin/team'
-
   get 'admin/team_post'
-
   get 'notification/index'
-
   get 'notification/show'
-
   get 'score/score'
 end
