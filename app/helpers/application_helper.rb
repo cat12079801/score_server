1075e6f78c5cbd9b96646e5e9d99ceae88a96f14
cat12079801 <cat12079801@gmail.com>
2015-09-07 17:53:29 +0900

    チームの点数計算用のヘルパー追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def team_point team
+    base = team.problems.pluck(:point).sum
+    team.user.each do |user|
+      user.problem.each do |p|
+        base += p.point / 100
+      end
+    end
+
+    base
+  end
+
+  def team_point_base team
+    team.problems.pluck(:point).sum
+  end
+
+  def team_point_special team
+    special = 0
+    team.user.each do |user|
+      user.problem.each do |p|
+        special += p.point / 100
+      end
+    end
+    special
+  end
 end
