d707acefd9575dc009d31d72237eaa2ed3437be7
cat12079801 <cat12079801@gmail.com>
2015-09-12 23:26:27 +0900

    team modelのvalidationが狂ってたので修正

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,4 @@ class Team < ActiveRecord::Base
   has_many :team_solve
 
   validates :name, presence: true, length: {in: 1..255}
-  #validates :solved_problem, length: {minimum: 0}
-  validates :point, numericality: {greater_than_or_equal_to: 0, only_integer: true}
 end
