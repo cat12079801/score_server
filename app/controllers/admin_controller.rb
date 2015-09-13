b7be6efe5eb34d0622e33b58ef0be46bec0caab1
cat12079801 <cat12079801@gmail.com>
2015-09-13 11:51:08 +0900

    ランキングで同点は同順位にするよう修正

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,17 +19,29 @@ class AdminController < ApplicationController
   end
 
   def score
-    @teams = {}
+    teams = []
     Team.all.each do |team|
-      @teams.merge!({team.name => (ApplicationController.helpers.team_point team)})
+      teams.push([(ApplicationController.helpers.team_point team), team.name])
     end
-    @teams.sort.reverse!
+    teams.sort!.reverse!
+    @teams = []
+    teams.each.with_index(1) do |team, i|
+      rank = (@teams.present? and team[0] == @teams[-1][2]) ? @teams[-1][0] : i
+      @teams.push([rank, team[1], team[0]])
+    end
+    @teams
 
-    @users = []
+    users = []
     User.where(admin_flag: false).each do |user|
-      @users.push([(ApplicationController.helpers.user_point user), user.account, user.team.name])
+      users.push([(ApplicationController.helpers.user_point user), user.account, user.team.name])
     end
-    @users.sort!.reverse!
+    users.sort!.reverse!
+    @users = []
+    users.each.with_index(1) do |user, i|
+      rank = (@users.present? and user[0] == @users[-1][3]) ? @users[-1][0] : i
+      @users.push([rank, user[1], user[2], user[0]])
+    end
+    @users
   end
 
   def team
--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,9 +1,15 @@
 class ScoreController < ApplicationController
   def score
-    @teams = {}
+    teams = []
     Team.all.each do |team|
-      @teams.merge!({team.name => (ApplicationController.helpers.team_point team)})
+      teams.push([(ApplicationController.helpers.team_point team), team.name])
     end
-    @teams.sort.reverse!
+    teams.sort!.reverse!
+    @teams = []
+    teams.each.with_index(1) do |team, i|
+      rank = (@teams.present? and team[0] == @teams[-1][2]) ? @teams[-1][0] : i
+      @teams.push([rank, team[1], team[0]])
+    end
+    @teams
   end
 end
